c32b624a9c5881c15f1a99b2ce19cbb57537623f
zax00000 <mateusz.swiderski123@gmail.com>
2026-03-14 19:30:34 +0100

    Correcting Overshoot

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -55,8 +55,12 @@ void AMovingPlatform::MovePlatform(float DeltaTime)
 		FString PlatformName = GetName();
 		UE_LOG(LogTemp, Display, TEXT("%s overshoot by %f"), *PlatformName, Overshoot);
 
+		FVector MoveDirection = PlatformVelocity.GetSafeNormal();
+		FVector NewStartLocation = StartLocation + MoveDirection * MoveDistance;
+		SetActorLocation(NewStartLocation);
+		StartLocation = NewStartLocation;
+
 		PlatformVelocity = -PlatformVelocity;
-		StartLocation = CurrentLocation;
 	}
 }
 
